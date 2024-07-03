77827e2aa79a34361c5b98766f9f928ae2c6b460
Samir M <67711948+samirm00@users.noreply.github.com>
2024-07-03 17:11:25 +0200

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,36 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// Construct path
+const __filename = fileURLToPath(import.meta.url);
+const PATH = dirname(__filename);
+dotenv.config({ path: path.join(PATH, '../.env') });
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 10
+});
+
+console.log('MySQL Pool created successfully');
+
+// Create query
+const query = async (sql, values) => {
+    const connection = await pool.getConnection();
+    try {
+        const [results] = await connection.query(sql, values);
+        return results;
+    } catch (err) {
+        return err;
+    } finally {
+        if (connection) {
+            connection.release();
+        }
+    }
+};
+
+export default query;
---
+++ routes/recipe.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import recipeControllers from '../controllers/recipe.js';
+import verifyToken from '../middleware/verifyToken.js';
+import recipeControllers from '../controllers/recipe.js';
+
+const router = express.Router();
+
+// routes
+
+export default router;

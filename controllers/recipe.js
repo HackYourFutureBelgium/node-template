580249e3ca77dff1481b2a23584a3f6a32723038
Hitomi <hitomi0727pupil@gmail.com>
2024-07-11 22:26:59 +0200

    add env, controllers and models

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -1,23 +1,49 @@
 import query from '../config/db.js';
+import { createRecipe, deleteRecipe, getRecipeById, listRecipes, updateRecipe } from '../models/recipe.js';
 
 const recipeControllers = {
     getAllRecipes: async (req, res) => {
         res.render('home', {
             title: 'Recipe',
-            path: '/recipes'
+            path: '/recipes',
+            recipes: listRecipes()
         });
     },
     getOneRecipe: async (req, res) => {
         const { id } = req.params;
         res.render('recipe', {
             id: id,
-            title: 'Book',
-            path: '/recipes'
+            title: 'Recipe',
+            path: '/recipes',
+            recipe: getRecipeById()
         });
     },
-    postRecipe: async (req, res) => {},
-    updateRecipe: async (req, res) => {},
-    deleteRecipe: async (req, res) => {},
+    postRecipe: async (req, res) => {
+        createRecipe() // how to get title and user_id from user?
+        res.render('home', {
+            title: 'Recipe',
+            path: '/recipes',
+            recipes: listRecipes()
+        });
+    },
+    updateRecipe: async (req, res) => {
+        const { id } = req.params;
+        res.render('recipe', {
+            id: id,
+            title: 'Recipe',
+            path: '/recipes/update',
+            recipe: updateRecipe()
+        });
+    },
+    deleteRecipe: async (req, res) => {
+        const { id } = req.params;
+        res.render('recipe', {
+            id: id,
+            title: 'Recipe',
+            path: '/recipes/delete',
+            recipe: deleteRecipe()
+        });
+    },
 };
 
 export default recipeControllers;
--- models/recipe.js
+++ models/recipe.js
@@ -2,6 +2,57 @@ import query from '../config/db.js';
 
 const createRecipeTable = async () => {
     try {
+        const queryStr = `CREATE TABLE recipes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    user_id INT,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+    );`
+        await query(queryStr)
+    } catch (err) {}
+};
+
+export const createRecipe = async (title, user_id) => {
+    try {
+        const queryStr = `INSERT INTO Recipe.recipes
+    (title, user_id)
+    VALUES(?, ?);`
+        const recipe = await query(queryStr, [title, user_id])
+        return recipe;
+    } catch (err) {}
+};
+
+export const updateRecipe = async (title, user_id, id) => {
+    try {
+        const queryStr = `UPDATE Recipe.recipes
+    SET title=?, user_id=?
+    WHERE id=?;`
+    const recipe = await query(queryStr, [title, user_id, id])
+    } catch (err) {}
+};
+
+export const deleteRecipe = async (id) => {
+    try {
+        const queryStr = `DELETE FROM Recipe.recipes
+WHERE id=?;`
+    const recipe = await query(queryStr, id)
+    } catch (err) {}
+};
+
+export const getRecipeById = async (id) => {
+    try {
+        const queryStr = `SELECT title, user_id
+        FROM Recipe.recipes
+        WHERE id=?;`
+        const recipe = await query(queryStr, id)
+    } catch (err) {}
+};
+
+export const listRecipes = async () => {
+    try {
+        const queryStr = `SELECT id, title, user_id
+FROM Recipe.recipes;`
+const recipe = await query(queryStr)
     } catch (err) {}
 };
 

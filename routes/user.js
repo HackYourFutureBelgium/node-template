ae3b8770beed863d118cc13002f9d31c5bc406f7
Hammad1820 <48063834+Hammad1820@users.noreply.github.com>
2024-07-13 20:43:33 +0200

    update

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,8 @@ const router = express.Router();
 
 
 // routes
+router.post('/register', userControllers.register);
+router.post('/login', userControllers.login);
+router.post('/logout', userControllers.logout);
 
 export default router;

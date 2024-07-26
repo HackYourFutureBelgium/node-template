cce06e138cfe7ff9d1406b1dbc3d2cc68f4eb496
gemechis <fikadugemechisgelana@gmail.com>
2024-07-26 18:28:41 +0200

    final

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -1,11 +1,35 @@
 import query from '../config/db.js';
 
 const recipeControllers = {
-    getAllRecipes: async (req, res) => {},
+    getAllRecipes: async (req, res) => {
+        try {
+            const { output } = await query('SELECT * FROM recipes');
+            res.status(200).json(output);
+        } catch (error) {
+            res.status(500).json({error: 'Failed to retrieve recipes'});
+        }
+    },
     getOneRecipe: async (req, res) => {},
-    postRecipe: async (req, res) => {},
+    postRecipe: async (req, res) => {
+        const { name, description } = req.body;
+        const newRecipe = await query(
+            'INSERT INTO recipes (name, description) VALUES (?, ?)',
+            [name, description]
+        );
+        res.json({ id: newRecipe.insertId, name, description });
+    },
     updateRecipe: async (req, res) => {},
-    deleteRecipe: async (req, res) => {},
+    deleteRecipe: async (req, res) => {
+        try {
+            const [result] = await query('DELETE FROM recipes WHERE id = ?', [id]);
+
+            if (result.affectedRows === 0) {
+                const error = new Error(`A recipe with the id of ${id} was not found`);
+                error.status = 404;
+                return next(error);
+            }
+
+    },
 };
 
 export default recipeControllers;
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,11 @@ import userControllers from '../controllers/user.js';
 
 const router = express.Router();
 
-
 // routes
+router.post('/register', userControllers.register);
+
+router.post('/login', userControllers.login);
+
+router.post('/logout', userControllers.logout);
 
 export default router;
